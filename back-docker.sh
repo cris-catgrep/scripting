0e0a96f12ae40230443d621859aad2e7c45d5a15
cris-catgrep <cristobal.espinosa@protonmail.com>
2024-06-20 17:46:17 -0500

    Avance del script

--- back-docker.sh
+++ back-docker.sh
@@ -3,5 +3,13 @@
 # Ciclo for que imprime el directorio donde se encuentran los archivos docker-compose de contenedor ejecutandose
 for dir in $(sudo docker compose ls | awk 'NR>1 { print $3 }'); do
     # Retirar permiso sudo a docker
-    dirname "$dir"
+    # Guarda una variable con el directorio del docker compose, luego hace cd al director y detiene todo el compose
+    directory=$(dirname "$dir")
+    cd "$directory" #&& sudo docker compose stop
+    
+    for cont in $(sudo docker compose ps -a | awk 'NR>1 { print $1 }'); do
+        echo $cont 
+        sudo docker inspect --format='{{range .Mounts}}{{println .Source .Destination}}{{end}}' "$cont"
+
+    done
 done
