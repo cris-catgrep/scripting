19d25d295d651d38730299c2797f3c0e05bde0c6
cris-catgrep <cristobal.espinosa@protonmail.com>
2024-06-25 22:12:01 -0600

    Script casi terminado

--- back-docker.sh
+++ back-docker.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+backup_dir=/home/$USER/backup
+
+mkdir -p $backup_dir
 
 # Ciclo for que imprime el directorio donde se encuentran los archivos docker-compose de contenedor ejecutandose
 for dir in $(sudo docker compose ls | awk 'NR>1 { print $3 }'); do
@@ -7,8 +10,19 @@ for dir in $(sudo docker compose ls | awk 'NR>1 { print $3 }'); do
     directory=$(dirname "$dir")
     cd "$directory" #&& sudo docker compose stop
     
+    # Ciclo en el que ira por cada compose activo o detenido
     for cont in $(sudo docker compose ps -a | awk 'NR>1 { print $1 }'); do
-        echo $cont 
-        sudo docker inspect --format '{{range .Mounts}} {{.Source}} {{.Destination}} {{println}}{{end}}'
+        if [[ -z "$vol" ]]; then
+            echo "$cont"
+        elif [[ -d "$vol" ]]; then
+            sudo docker compose stop
+            if [[ "$vol" == "$directory"* ]]; then
+                sudo docker run --rm -v "$vol":/volume -v "$backup_dir":/backup alpine tar -czf /backup/$cont.tar.gz -C /volume ./
+            else
+                echo "nada"
+            fi
+        fi
+
+        done
     done
 done
